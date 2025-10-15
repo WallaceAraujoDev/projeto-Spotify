00d37548c4c999ad98f0fabaabc4a63aba7e44ab
WallaceAraujoDev <wallacearaujo.dev@hotmail.com>
2025-10-14 21:42:46 -0300

    att js

---
+++ scripts.js
@@ -0,0 +1,19 @@
+// Array de artistas
+const artists = [
+  { name: 'Morrique & Juliano', image: './img/artista-henrique-juliano.jpg' },
+  { name: 'Jorge & Mateu', image: './img/artista-jorge-mateus.jpg' },
+  { name: 'Zé Neto & Cristiano', image: './img/artista-ze-neto.jpg' },
+  { name: 'Gustavo Lima', image: './img/artista-gustavo-limma.jpg' },
+  { name: 'Luan Santana', image: './img/artista-luan-santana.jpg' },
+  { name: 'Matheus & Kauan', image: './img/artista-mateus-kauan.jpg' }
+];
+
+// Array de álbuns
+const albumData = [
+  { name: 'Molto Molto (Sleep & Relaxation Sounds)', artist: 'Sleepy John', image: './img/album-white-noise.jpg' },
+  { name: 'O del Espíldia Todo (Ao Vivo)', artist: 'Morrique & Juliano', image: './img/album-caju.jpg' },
+  { name: 'Nada como un día após o outro', artist: 'Reclama\'s', image: './img/album-vida-loka.jpg' },
+  { name: 'HIIT ME NANO AND SOFT', artist: 'Billie Ellian', image: './img/album-hit-me.jpg' },
+  { name: 'COJO', artist: 'Linker', image: './img/album-racionais.jpg' },
+  { name: 'Escándalo Intimo', artist: 'Luisa Souza', image: './img/album-escandalo.jpg' }
+];
